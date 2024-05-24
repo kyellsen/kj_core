1be6c5c04021fb1fef4573c13a8d06b5225461e8
Kyell Jensen <mail@kyelljensen.de>
2024-05-24 23:36:43 +0200

    Entscheidung für Dummy Variablen für die Sensoren und verzicht auf Mixed effects modellierung. m_frequency entfernt, da unnötiger Code ohne zusatznutzen

--- kj_core/classes/similarity_metrics.py
+++ kj_core/classes/similarity_metrics.py
@@ -5,21 +5,42 @@ from scipy.stats import pearsonr
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from dataclasses import dataclass
 
-
 @dataclass
 class SimilarityMetrics:
-    pearson_r: float
-    p_value: float
-    r2: float
-    mse: float
-    rmse: float
-    mae: float
+    pearson_r: float  # Pearson correlation coefficient
+    p_value: float  # p-value of the Pearson correlation
+    r2: float  # R-squared, coefficient of determination
+    mse: float  # Mean Squared Error
+    rmse: float  # Root Mean Squared Error
+    nrmse: float  # Normalized RMSE (Normalized Root Mean Squared Error)
+    cv: float  # Coefficient of Variation
+    mae: float  # Mean Absolute Error
+    nmae: float  # Normalized MAE (Normalized Mean Absolute Error)
 
     def to_dict(self) -> Dict[str, float]:
+        """
+        Convert the SimilarityMetrics dataclass to a dictionary.
+
+        Returns:
+            Dict[str, float]: A dictionary representation of the SimilarityMetrics.
+        """
         return self.__dict__
 
     @classmethod
     def calc(cls, series1: pd.Series, series2: pd.Series) -> "SimilarityMetrics":
+        """
+        Calculate similarity metrics between two pandas Series.
+
+        Args:
+            series1 (pd.Series): The first series of values.
+            series2 (pd.Series): The second series of values.
+
+        Returns:
+            SimilarityMetrics: An instance of the SimilarityMetrics dataclass containing all the calculated metrics.
+
+        Raises:
+            ValueError: If either of the input series contains NaN values.
+        """
         if series1.isnull().any() or series2.isnull().any():
             raise ValueError("Input series must not contain NaN values.")
 
@@ -27,9 +48,12 @@ class SimilarityMetrics:
         r2 = r2_score(series1, series2)
         mse = mean_squared_error(series1, series2)
         rmse = np.sqrt(mse)
+        nrmse = rmse / (series1.max() - series1.min())
+        cv = rmse / series1.mean()
         mae = mean_absolute_error(series1, series2)
+        nmae = mae / (series1.max() - series1.min())
 
-        return cls(pearson_r, p_value, r2, mse, rmse, mae)
+        return cls(pearson_r, p_value, r2, mse, rmse, nrmse, cv, mae, nmae)
 
     def check_thresholds(self, metrics_warning: Dict[str, Tuple[Optional[float], Optional[float]]]) -> List[str]:
         """
@@ -48,6 +72,5 @@ class SimilarityMetrics:
             metric_value = getattr(self, metric, 0)
             if (lower_threshold is not None and metric_value < lower_threshold) or \
                (upper_threshold is not None and metric_value > upper_threshold):
-                warnings.append(f"Metric '{metric}' value {metric_value} outside of threshold range {lower_threshold}-{upper_threshold}")
+                warnings.append(f"Metric '{metric}' value {metric_value:.4f} outside of threshold range {lower_threshold}-{upper_threshold}")
         return warnings
-
